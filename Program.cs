5ca524c1710e4afed2cabcc09f0e36e82781a014
John De Leon <johnd20@109d25lx1sdf8j7.student.cssd.ab.ca>
2019-02-26 14:55:57 -0700

    Updated

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace coinflip
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // int coin;
+            // string userGuess;
+            // Random rng = new Random();
+
+            // Console.Write(" Heads or Tails, sir ?");
+            // userGuess = Console.ReadLine();
+
+            // coin = rng.Next(0, 2);
+
+            // if(coin == 0 && userGuess == "tails") 
+            // {
+            //     Console.WriteLine("Baby Food!");
+            // }
+            // else if (coin == 1 && userGuess == "heads")
+            // {
+            //     Console.WriteLine("baby food!");
+            // }
+            // else
+            // {
+            //     if(coin == 0 && userGuess == "heads")
+            //     {
+            //         Console.WriteLine("The coin flip was Tails, you lose!");
+            //     }
+            //     else if (coin == 1 && userGuess == "tails")
+            //     {
+            //         Console.WriteLine("The coin flip was Heads, you lose!");
+            //     }
+        
+            // }
         }
+
     }
 }
